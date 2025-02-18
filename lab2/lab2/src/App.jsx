31badb0e1362959209aec8d60d4f05fdf30ee5ba
Zlata <algoneuro1@gmail.com>
2025-02-19 01:56:35 +0700

    Save repo

---
+++ lab2/lab2/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from './components/Button'
+import Container from './components/Container'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+const saySomething = (text) => {
+  console.log(text)
+}
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite !!! React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Container>
+        <Button label='Some text' onClick={() => saySomething('Button pressed')} />
+        <Button label='Say Hello' onClick={() => saySomething('Hello World!')} />
+      </Container>
+    </>
+  )
+}
+
+export default App
---
+++ lab2/lab2/src/components/Container.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Container = ({children}) => {
+     return <div style={{border: "solid 1px red"}}>{children}</div>
+}
+export default Container;
